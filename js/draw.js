c7001037b96a0487611da7e55958f6e595e872ab
Daniel Milde <daniel@milde.cz>
2011-10-23 00:06:50 +0200

    Compile JS using google closure compiler

---
+++ js/draw.js
@@ -0,0 +1,129 @@
+function Draw(element) {
+	"use strict";
+	var self = this;
+
+	self.element = element;
+	self.socket = false;
+	self.isDown = false;
+	self.line = [];
+
+	self.init();
+}
+
+Draw.prototype.init = function () {
+	"use strict";
+	var self = this;
+
+	self.ctx = self.element.getContext("2d");
+	self.ctx.canvas.width  = window.innerWidth;
+	self.ctx.canvas.height = window.innerHeight;
+	self.lastPoint = {};
+};
+
+Draw.prototype.getX = function (event) {
+	"use strict";
+	var self = this;
+
+	return event.pageX - self.element.offsetLeft;
+};
+
+Draw.prototype.getY = function (event) {
+	"use strict";
+	var self = this;
+
+	return event.pageY - self.element.offsetTop;
+};
+
+Draw.prototype.setSocket = function (socket) {
+	"use strict";
+	var self = this;
+
+	self.socket = socket;
+
+	self.socket.on('add', function (data) {
+		data.lines.forEach(function (line) {
+			self.ctx.beginPath();
+			line.points.forEach(function (item) {
+				self.ctx.lineTo(item.x, item.y);
+			});
+			self.ctx.strokeStyle = "#555";
+			self.ctx.stroke();
+		});
+	});
+};
+
+Draw.prototype.mouseDown = function (event) {
+	"use strict";
+	var self = this, x, y;
+
+	self.isDown = true;
+
+	x = self.getX(event);
+	y = self.getY(event);
+
+	self.lastPoint = {x: x, y: y};
+
+	self.line = [{x: x, y: y}];
+};
+
+Draw.prototype.mouseMove = function (event) {
+	"use strict";
+	var self = this, x, y;
+
+	if (!self.isDown) {
+		return;
+	}
+
+	x = self.getX(event);
+	y = self.getY(event);
+
+	self.ctx.beginPath();
+	self.ctx.moveTo(self.lastPoint.x, self.lastPoint.y);
+	self.ctx.lineTo(x, y);
+	self.ctx.strokeStyle = "#000";
+	self.ctx.stroke();
+
+	self.lastPoint = {x: x, y: y};
+
+	self.line.push({x: x, y: y});
+};
+
+Draw.prototype.mouseUp = function (event) {
+	"use strict";
+	var self = this;
+
+	if (self.line.length && self.socket) {
+		self.socket.emit('add', {lines: [{points: self.line}]});
+	}
+
+	self.isDown = false;
+	self.line = [];
+	self.lastPoint = {};
+};
+
+$(function () {
+	"use strict";
+	var draw = new Draw(document.getElementById("canvas"));
+
+	$('#canvas').mousedown(function (event) {
+		draw.mouseDown(event);
+	});
+	$('#canvas').mousemove(function (event) {
+		draw.mouseMove(event);
+	});
+	$('#canvas').mouseup(function (event) {
+		draw.mouseUp(event);
+	});
+	$('#canvas').mouseout(function (event) {
+		draw.mouseUp(event);
+	});
+
+	$('#help').click(function () {
+		$('#help span').dialog();
+	});
+
+	$.getJSON("./config.json", function (config) {
+		var socket = io.connect("http://" + window.location.hostname + ':' + config.port);
+		draw.setSocket(socket);
+	});
+});
---
+++ tools/compile.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+BASE_PATH=~/workspace/js/closure
+CALCDEPS_PATH=$BASE_PATH/closure-library/closure/bin/  #directory containing calcdeps.py
+JAR_PATH=$BASE_PATH                                    #directory containing compiler.jar
+CLOSURE_PATH=$BASE_PATH/closure-library                #path to closure-library
+#COMPILER_FLAGS='--compiler_flags --compilation_level=ADVANCED_OPTIMIZATIONS'
+
+if [ -z $1 ]; then
+	echo "Usage:" $0 "file.js"
+	exit 1
+fi
+
+INPUTS=""
+for IN in $@; do
+	INPUTS="${INPUTS} -i ${IN}"
+done
+
+$CALCDEPS_PATH/calcdeps.py $INPUTS \
+	-p $CLOSURE_PATH -o compiled \
+	$COMPILER_FLAGS \
+	-c $JAR_PATH/compiler.jar > compiled-$1
